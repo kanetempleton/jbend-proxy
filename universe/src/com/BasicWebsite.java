6f1aef0310a028d1dd9682caa274aeeba86f8198
kanetempleton <ktempleton96@yahoo.com>
2022-08-26 21:46:31 -0500

    updating

---
+++ universe/src/com/BasicWebsite.java
@@ -0,0 +1,17 @@
+package com;
+
+public class BasicWebsite extends NetApplication {
+
+    public BasicWebsite() {
+        super("mySite");
+    }
+
+    public void runApplication() {
+        System.out.println("Appleecashun is runnin!");
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- universe/src/com/Main.java
+++ universe/src/com/Main.java
@@ -10,108 +10,15 @@ import com.db.crud.*;
 import java.lang.reflect.Field;
 
 
-
-
-
 public class Main {
 
     public static Launcher launcher;
-    public static HTTP http_protocol;
 
     public static void main(String[] args) {
-      //  System.out.println("main'd");
-
-        launcher = new Launcher();
-        launcher.DEBUG_SERVER_LEVEL = 0;
-       //./ launcher.addDatabaseManager();
-        //launcher.addLoginHandler();
-        //launcher.addCareTaker(1800000);
-
-        CRUDHandler.DEBUG_CRUD = true;
-
-
-       // TicketProcessing ticketManager = new TicketProcessing();
-     //   ticketManager.assignClass(Ticket.class);
-
-
-       /* WebPackets wp = new WebPackets() {
-            public void processPOST(ServerConnection c, String uri, int packetID, String[] fields, String[] values) {
-                if (packetID >= 500 && packetID <= 600) {
-        //            ticketManager.processPOST(http_protocol,c,uri,packetID,fields,values);
-                }
-            }
-        };
-        Server tcp = new Server(new TCP(43594),1024);
-        tcp.setWebPacketHandler(wp);*/
-      //  TCPProxy proxy = new TCPProxy(80);
-      //  proxy.mapPort("127.0.0.1",8888);
-      //  Server tcpproxy = new Server(proxy,2048);
-
-
-
-        //TaskHandler<Task> H = new TaskHandler();
-       // H.assignClass(Task.class);
-
-       // Task T = new Task(H,"jizz");
-       // H.create(T);
-
-       // launcher.loadThread(http,"Web Server");
-       // launcher.loadThread(tcp,"TCP Server");
-        //launcher.loadThread(tcpproxy,"Proxy Server");
-
-        //TODO: figure out why this breaks everything
-        //whichever of these two threads that loads first becomes the only thread now
-        // or something bro wtf idk i wanna go home
-        // WAIT NEVERMIND HOLY SHIT I GOT IT
-        //I TOTALLY FORGOT ABOUT THIS LITTLE GUY
-        //EXTREMELYYYY IMPORTANT!!!!!
-        // Main.launcher.nextStage();
-       // launcher.loadThread(H,"TaskManager"); //FUCK YEAH!!!!!!!!!!!!!
-
-
-       // launcher.loadThread(ticketManager,"Ticketing System");
-
-       // H.create(T);
-
-        //ok fuck it... done for today.
-        // CREATE: [x]tables, [x]objects
-        // READ: [x]tables, [*]objects
-        // UPDATE: [*]tables, []objects
-        // DELETE: [x]tables, []objects
-
-        /* what i got done:
-         - created query for check table existence
-         - created query for create table
-         - created query for delete table
-         - CRUD for those three as well
-         ~~ next day ~~
-         - generate table structure strings for both database and class
-         - check if table structures match
-         - auto add/drop of columns to sync database with class
-         - identify name/type mismatch in structures (still needs to be handled)
-         - query: update table (add/drop columns)
-         - query: get table info (column names and data types)
-         - fixed some bugs
-         ~~ day 3 ~~
-         - query: insert into table
-         - query: select from table
-
-         todo:
-         - query: update object in table
-         - query: delete object in table
-         - then the bs with type checks yeah yeah... we doing fine.
-         - CRUD: [table] handle name/type mismatches in schema
-         - table.read(): need a way to make sure data is prepared before used
-         */
-
-        //start the handler for tasks
-        //Task T = new Task(H,"finish the task system");//H.create(); //no don't use H.create() who the fuck cares
-
-        // new Thread(H).start();
-        //launcher.loadThread(H,"TaskManager");
-       // launcher.startThreads();
+        BasicWebsite mysite = new BasicWebsite();
+        launcher = new Launcher(mysite);
+        launcher.startThreads();
     }
-
 
 
 }
